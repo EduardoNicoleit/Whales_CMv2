7c982c746ba819fed7bc893eac58eaa17a00c983
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2022-01-12 08:02:34 -0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -18,7 +18,8 @@
   // Customise the app by changing the following variables.
   const TITLE = "Solana HODL Whales";
   const DESCRTIPTION = "BREED | HODL | EARN";
-  const HEADER_TITLE = "Solana HODL Whales";
+  const HEADER_TITLE = "Being the only source of life force which powers our whales, KRILL enables the whales to travel
+across the solaxy and is rightfully owned by every HODL whale.";
   const HEADER_LINK = "https://solanahodlwhales.io/";
   // Your image or GIF needs to be in the /public folder for this to work
   const IMAGE_LINK = "https://pbs.twimg.com/profile_images/1475595857810509824/be89Yyda_400x400.jpg";

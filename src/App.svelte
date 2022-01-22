0b214e71c556b81b5c856ad117257a968979beed
Eduardo Nicoleit <eduardonicoleit@hotmail.com>
2022-01-22 13:26:17 -0300

    Navbar

--- src/App.svelte
+++ src/App.svelte
@@ -123,6 +123,49 @@
     </div>
   {:else}
     <!-- Menu Bar -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+       <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+   
+       <input type="checkbox" id="active">
+       <label for="active" class="menu-btn"><i class="fas fa-bars"></i></label>
+       <div class="wrapper">
+         <ul>
+   <li><a href="#">Home</a></li>
+   <li><a href="#">About</a></li>
+   <li><a href="#">Services</a></li>
+   <li><a href="#">Gallery</a></li>
+   <li><a href="#">Feedback</a></li>
+   </ul>
+   </div>
+   <div class="content">
+         <div class="title">
+   Fullscreen Overlay Navigation Bar</div>
+   <p>
+   with HTML & CSS Neon Effect</p>
+       </div>
+     <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+       <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+
+   
+       <input type="checkbox" id="active">
+       <label for="active" class="menu-btn"><i class="fas fa-bars"></i></label>
+       <div class="wrapper">
+         <ul>
+   <li><a href="#">Home</a></li>
+   <li><a href="#">About</a></li>
+   <li><a href="#">Services</a></li>
+   <li><a href="#">Gallery</a></li>
+   <li><a href="#">Feedback</a></li>
+   </ul>
+   </div>
+   <div class="content">
+         <div class="title">
+   Fullscreen Overlay Navigation Bar</div>
+   <p>
+   with HTML & CSS Neon Effect</p>
+   </div>
 
     {#if HEADER_TITLE}
       <a
